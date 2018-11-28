ca8bb5c85e79978cb109a8ca5cfb975045cb344e
kinga-mate-tech <git-sync@trilby.media>
2018-11-28 18:08:31 +0800

    (Grav GitSync) Automatic Commit from kinga-mate-tech

---
+++ plugins/form/app/fields/index.js
@@ -0,0 +1,5 @@
+import FileInstances from './file';
+import ArrayInstances from './array';
+import PageMedia, { Instance as PageMediaInstances } from './media';
+
+export default { FileInstances, ArrayInstances, Media: { PageMedia, PageMediaInstances } };
